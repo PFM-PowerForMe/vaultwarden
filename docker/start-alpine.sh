0ed41ee40321d3c2886f5edb753fb3f47f45bc30
John (Github) <48443038+wangxiaoerYah@users.noreply.github.com>
2025-05-06 01:15:08 +0800

    更新workflow

--- docker/start-alpine.sh
+++ docker/start-alpine.sh
@@ -2,6 +2,10 @@
 
 env >> /etc/default/locale
 
+if [ -n $ENCRYPTION_PUB_KEY ];then
+	echo "$ENCRYPTION_PUB_KEY" | gpg --import
+fi
+
 crond
 
 sh /start.sh
--- docker/start-debian.sh
+++ docker/start-debian.sh
@@ -2,6 +2,10 @@
 
 env >> /etc/default/locale
 
+if [ -n $ENCRYPTION_PUB_KEY ];then
+	echo "$ENCRYPTION_PUB_KEY" | gpg --import
+fi
+
 service cron start
 
 sh /start.sh
